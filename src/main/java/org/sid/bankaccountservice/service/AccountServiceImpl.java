5b3ad2b6b5111966d8735cb41484ab1c35513d30
Hamza Argane <hamza.argane@gmail.com>
2022-10-23 19:48:20 +0100

    Part 1

---
+++ src/main/java/org/sid/bankaccountservice/service/AccountServiceImpl.java
@@ -0,0 +1,38 @@
+package org.sid.bankaccountservice.service;
+
+import org.sid.bankaccountservice.mappers.AccountMapper;
+import org.sid.bankaccountservice.dto.BankAccountRequestDTO;
+import org.sid.bankaccountservice.dto.BankAccountResponseDTO;
+import org.sid.bankaccountservice.entities.BankAccount;
+import org.sid.bankaccountservice.repositories.BankAccountRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Service
+@Transactional
+public class AccountServiceImpl implements AccountService {
+    private BankAccountRepository bankAccountRepository;
+    private AccountMapper accountMapper;
+
+    public AccountServiceImpl(BankAccountRepository bankAccountRepository, AccountMapper accountMapper) {
+        this.bankAccountRepository = bankAccountRepository;
+        this.accountMapper = accountMapper;
+    }
+
+    @Override
+    public BankAccountResponseDTO addAccount(BankAccountRequestDTO bankAccountDTO) {
+        BankAccount bankAccount=BankAccount.builder()
+                .id(UUID.randomUUID().toString())
+                .balance(bankAccountDTO.getBalance())
+                .type(bankAccountDTO.getType())
+                .currency(bankAccountDTO.getCurrency())
+                .createdAt(new Date())
+                .build();
+        BankAccount saveBankAccount=bankAccountRepository.save(bankAccount);
+        BankAccountResponseDTO bankAccountResponseDTO=accountMapper.fromBankAccount(saveBankAccount);
+        return bankAccountResponseDTO;
+    }
+}
